7a946355c7b3820deef86ab84e84440a876087be
Kevin <kevin.tassi@gmail.com>
2020-01-16 21:56:11 +0100

    Render documents as todo list

--- app.js
+++ app.js
@@ -1,5 +1,31 @@
+const list = document.querySelector("#todo-list");
+
+function renderDocument(doc) {
+  let li = document.createElement("li");
+  li.setAttribute("data-id", doc.id);
+
+  let data = doc.data();
+  console.log("Data", data);
+
+  let checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
+  checkbox.setAttribute("name", data.title);
+  checkbox.setAttribute("value", doc.id);
+  checkbox.checked = data.is_done;
+
+  let title = document.createElement("span");
+  title.textContent = doc.data().title;
+
+  li.appendChild(checkbox);
+  li.appendChild(title);
+
+  return li;
+}
+
 db.collection("todo")
   .get()
-  .then(data => {
-    console.log("++", data);
+  .then(response => {
+    const items = [];
+    response.docs.forEach(doc => items.push(renderDocument(doc)));
+    items.map(item => list.appendChild(item));
   });
